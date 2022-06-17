1253c05cac249d00c76f7d04ad719172de5442db
Samuel Moraes <43325441+SamMoraes@users.noreply.github.com>
2022-06-17 11:27:21 -0300

    Projeto API Controle de Atendimento

---
+++ controle-de-atendimentos/src/main/java/br/com/sammoraes/repositories/IReceitaMedicaRepository.java
@@ -0,0 +1,9 @@
+package br.com.sammoraes.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.sammoraes.entities.ReceitaMedica;
+
+public interface IReceitaMedicaRepository extends CrudRepository<ReceitaMedica, Integer>{
+
+}
